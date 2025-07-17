9ff70c4d7a759f0c93883edfb4340f4341ce2b2e
KanavKapoor05 <kapoorkanav01@gmail.com>
2025-07-18 03:55:52 +0530

    added chatModel

---
+++ server/models/chatModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+  prompt: { type: String, required: true },
+  response: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
